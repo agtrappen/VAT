2a0937c71886dbf63514e7642173b827152df015
Alexander <agtrappen@hotmail.com>
2020-10-13 13:18:23 +0200

    added test files

---
+++ program/src/main/java/TestClass.java
@@ -0,0 +1,22 @@
+public class TestClass {
+    private String name;
+
+    public TestClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TesttClass{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
