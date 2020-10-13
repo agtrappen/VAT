88816e18716d5d73bcd0b7e58bc6234e422984de
Alexander <agtrappen@hotmail.com>
2020-10-13 13:09:59 +0200

    added test

--- program/src/main/java/Test.java
+++ program/src/main/java/Test.java
@@ -15,7 +15,7 @@ public class Test {
 
     @Override
     public String toString() {
-        return "Test{" +
+        return "Testt{" +
                 "name='" + name + '\'' +
                 '}';
     }
