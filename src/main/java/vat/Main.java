7c9e90aaf919b3a3f7ed9c0d5788b93090787e22
Alexander <agtrappen@hotmail.com>
2020-10-19 19:40:33 +0200

    added mainGUI

--- src/main/java/vat/Main.java
+++ src/main/java/vat/Main.java
@@ -1,6 +1,37 @@
 package vat;
 
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import vat.ui.MainUI;
+
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        MainUI mainView = new MainUI();
+
+        BorderPane layout = new BorderPane();
+        HBox menu = new HBox();
+        menu.setPadding(new Insets(20, 20, 20, 20));
+        menu.setSpacing(10);
+
+        Button enterButton = new Button("Enter new words");
+        Button practiceButton = new Button("Practice");
+
+        menu.getChildren().addAll(enterButton, practiceButton);
+        layout.setTop(menu);
+
+        layout.setCenter(mainView.getView());
+        Scene view = new Scene(layout, 400, 300);
+
+        stage.setScene(view);
+        stage.show();
     }
 }
