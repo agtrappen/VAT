7c2078e77c385e79100cccf044ef589507596b4a
Alexander <agtrappen@hotmail.com>
2020-10-20 18:24:03 +0200

    add edit bol view

--- src/main/java/vat/Main.java
+++ src/main/java/vat/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import vat.ui.BlockUI;
 import vat.ui.CylinderUI;
 import vat.ui.MainUI;
 
@@ -15,11 +16,13 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         MainUI mainView = new MainUI();
         CylinderUI cylinderView = new CylinderUI();
+        BlockUI blockView = new BlockUI();
 
         BorderPane layout = new BorderPane();
 
-        layout.setCenter(cylinderView.getView());
+        layout.setCenter(blockView.getView());
         Scene view = new Scene(layout, 400, 300);
+        stage.setTitle("VAT");
 
         stage.setScene(view);
         stage.show();
